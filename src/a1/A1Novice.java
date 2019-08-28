a0c79c15b57e04cabf7ca5ecc7f24fd5bc717ca4
itliu <itliu@itliu-pc.wireless-1x.unc.edu>
2019-08-27 23:00:43 -0400

    Wrote code, but need to figure out scanner order

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,53 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		//Integers
+		int count = scan.nextInt();
+		int[] values = new int[count];
 		
+		for (int i=0; i<values.length; i++) {
+			values[i] = scan.nextInt();
+		}
+		
+		//Strings
+		//String names = scan.next();
+		String[] fullname = new String[2];
+		
+		for (int j=0; j<2; j++) {
+			fullname[j] = scan.next();
+		}
+		
+		//Doubles
+		//double item = scan.nextDouble();
+		double[] price = new double[values.length - 2];
+		
+		for (int k=0; k<price.length; k++) {
+			price[k] = scan.nextDouble();
+		}
+		scan.close();
+		
+		double[] multiply = new double[price.length];
+		
+		for (int i=0; i<price.length; i++) {
+			multiply[i] = price[i] * values[i+2];
+		}
+		
+		double sum = calculateValueSum(multiply);
+		
+		for (int i=0; i<multiply.length; i++) {
+		System.out.println(fullname[0].charAt(0) + ". " + fullname[1] + " " + sum);
+		
+		}
 	}
+	static double calculateValueSum(double[] multiply) {
+		
+		double sum = 0;
+		
+		for (int i=0; i<multiply.length; i++) {
+			sum += multiply[i];
+		}
+		
+		return sum;
+	}
+	
 }
